d18e540ff80c9ad666fa6e4d2433f04b603e6ec1
Anthony Gress <the.techx.guy@gmail.com>
2026-04-23 03:18:46 -0400

    bump ver

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 return {
     name = "zen_ui",
-    version = "0.0.4",
+    version = "0.0.5",
     fullname = "Zen UI",
     description = "Minimal, performant, centralized KOReader UI plugin.",
 }
